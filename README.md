b968f9dfd1e669f826b7078f342e8ab7a6e00e28
Babish Shrestha <babishshrestha@gmail.com>
2015-04-08 13:58:49 +0545

    Express Paginate helper template

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
-# express-handlebars-paginate
+# Express Handlebars Paginate Helper with Twitter Bootstrap Enabled
+
+
+## Install
+
+    npm install express-handlebars-paginate
+
+## Usage
+
+```javascript
+/* ... */
+
+var paginateHelper = require('express-handlebars-paginate');
+//Register Helper
+Handlebars.registerHelper('paginate', paginate);
+```
+
+###With [Handlebars][]
+```javascript
+/* ... */
+Handlebars.registerHelper('paginate', paginate);
+```
+
+###With [Express Handlebars][]
+```javascript
+/* ... */
+hbs.handlebars.registerHelper('paginateHelper', paginateHelper.createPagination);
+```
+
+```handlebars Template
+{{{paginateHelper pagination}}}
+```
+
+[Express Handlebars]: https://github.com/ericf/express-handlebars
+[Handlebars]: https://github.com/wycats/handlebars.js
+
+## Options
+
+### limit
+Type: `int`
+Default: `7`
+
+Allows to define the number of page number limit
+
+```handlebars
+{{paginate pagination limit="7"}}
+```
+
+### leftText
+Type: `string`
+Default: `<i class="fa fa-chevron-left"></i>`
+
+Allows to define the text for left link
+
+```handlebars
+{{paginate pagination leftText="Prev"}}
+```
+
+### rightText
+Type: `string`
+Default: `<i class="fa fa-chevron-right"></i>`
+
+Allows to define the text for right link
+
+```handlebars
+{{paginate pagination rightText="Next"}}
+```
+
+### paginationClass
+Type: `string`
+Default: `pagination pagination-sm`
+
+Allows to define the ul pagination class
+
+```handlebars
+{{paginate pagination paginationClass="some class"}}
+```
---
+++ index.js
@@ -0,0 +1,72 @@
+/**
+ * Created by Babish Shrestha on 4/8/2015.
+ */
+"use strict";
+exports.createPagination = function (pagination, options) {
+    if (!pagination) {
+        return '';
+    }
+    var limit = 7
+        , n;
+    var page = pagination.page;
+    var leftText = '<i class="fa fa-chevron-left"></i>';
+    var rightText = '<i class="fa fa-chevron-right"></i>';
+    var paginationClass = 'pagination pagination-sm';
+
+    if (options.hash.limit) limit = +options.hash.limit;
+    if (options.hash.leftText) leftText = options.hash.leftText;
+    if (options.hash.rightText) rightText = options.hash.rightText;
+    if (options.hash.paginationClass) paginationClass = options.hash.paginationClass;
+
+    var pageCount = Math.ceil(pagination.totalRows / pagination.limit);
+
+    var template = '<ul class="' + paginationClass + '">';
+
+    // ========= Previous Button ===============
+    if (page === 1) {
+        n = 1;
+        template = template + '<li class="disabled"><a href="?page=' + n + '">'+ leftText +'</a></li>';
+    }
+    else {
+        n = page - 1;
+        template = template + '<li><a href="?page=' + n + '">'+ leftText +'</a></li>';
+    }
+
+    // ========= Page Numbers Middle ======
+
+    var i = 0;
+    var leftCount = Math.ceil(limit / 2) - 1;
+    var rightCount = limit - leftCount - 1;
+    if (page + rightCount > pageCount) {
+        leftCount = limit - (pageCount - page) - 1;
+    }
+    if (page - leftCount < 1) {
+        leftCount = page - 1;
+    }
+    var start = page - leftCount;
+
+    while (i < limit && i < pageCount) {
+        n = start;
+        if (start === page) {
+            template = template + '<li class="active"><a href="?page=' + n + '">' + n + '</a></li>';
+        } else {
+            template = template + '<li><a href="?page=' + n + '">' + n + '</a></li>';
+        }
+
+        start++;
+        i++;
+    }
+
+// ========== Next Buton ===========
+    if (page === pageCount) {
+        n = pageCount;
+        template = template + '<li class="disabled"><a href="?page=' + n + '">'+ leftText +'</i></a></li>';
+    }
+    else {
+        n = page + 1;
+        template = template + '<li><a href="?page=' + n + '">'+ leftText +'</a></li>';
+    }
+    template = template + '</ul>';
+    return template;
+}
+;
