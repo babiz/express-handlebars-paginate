8b3b30756c970b6af1a17265f4c67809e21386ae
Afu Tse <solapa@gmail.com>
2019-09-14 10:58:26 -0500

    fix pagination and add first and last page

--- index.js
+++ index.js
@@ -1,54 +1,68 @@
 /**
- * Created by Babish Shrestha on 4/8/2015.
+ * Modified by Afu Tse on 14/11/2019.
  */
 "use strict";
-exports.createPagination = function (pagination, options) {
+exports.createPagination = (pagination, options) => {
+
     if (!pagination) {
         return '';
     }
 
-    var limit = 7
-        , n;
-    var queryParams='';
-    var page = pagination.page;
-    var leftText = '<i class="fa fa-chevron-left"></i>';
-    var rightText = '<i class="fa fa-chevron-right"></i>';
+    var limit = 7;
+    var n = 1;
+    var queryParams = '';
+    var page = parseInt(pagination.page || 0);
+    var leftText = '<i class="fas fa-angle-left"></i>';
+    var rightText = '<i class="fas fa-angle-right"></i>';
+    var firstText = '<i class="fas fa-angle-double-left"></i>';
+    var lastText = '<i class="fas fa-angle-double-right"></i>';
     var paginationClass = 'pagination pagination-sm';
 
     if (options.hash.limit) limit = +options.hash.limit;
     if (options.hash.leftText) leftText = options.hash.leftText;
     if (options.hash.rightText) rightText = options.hash.rightText;
+    if (options.hash.firstText) firstText = options.hash.firstText;
+    if (options.hash.lastText) lastText = options.hash.lastText;
     if (options.hash.paginationClass) paginationClass = options.hash.paginationClass;
 
     var pageCount = Math.ceil(pagination.totalRows / pagination.limit);
 
     //query params
-    if(pagination.queryParams){
+    if (pagination.queryParams) {
         queryParams = '&';
         for (var key in pagination.queryParams) {
             if (pagination.queryParams.hasOwnProperty(key) && key !== 'page') {
-                queryParams += key+"="+pagination.queryParams[key]+"&";
+                queryParams += key + "=" + pagination.queryParams[key] + "&";
             }
         }
-        var lastCharacterOfQueryParams = queryParams.substr(queryParams.length-1,1);
+        var lastCharacterOfQueryParams = queryParams.substring(queryParams.length, -1);
 
-        if(lastCharacterOfQueryParams === "&"){
+        if (lastCharacterOfQueryParams === "&") {
             //trim off last & character
-            queryParams = queryParams.substring(0,queryParams.length-1);
+            queryParams = queryParams.substring(0, queryParams.length - 1);
         }
     }
 
-
     var template = '<ul class="' + paginationClass + '">';
+
+    // ========= First Button ===============
+    if (page === 1) {
+        template = template + '<li class="page-item disabled"><a class="page-link" href="?page=1' + queryParams + '">' + firstText + '</a></li>';
+    } else {
+        template = template + '<li class="page-item"><a class="page-link" href="?page=1' + queryParams + '">' + firstText + '</a></li>';
+    }
 
     // ========= Previous Button ===============
     if (page === 1) {
         n = 1;
-        template = template + '<li class="disabled"><a href="#">'+ leftText +'</a></li>';
-    }
-    else {
-        n = page - 1;
-        template = template + '<li><a href="?page=' + n + queryParams + '">'+ leftText +'</a></li>';
+        template = template + '<li class="page-item disabled"><a class="page-link" href="?page=' + n + queryParams + '">' + leftText + '</a></li>';
+    } else {
+        if (page <= 1) {
+            n = 1;
+        } else {
+            n = page - 1;
+        }
+        template = template + '<li class="page-item"><a class="page-link" href="?page=' + n + queryParams + '">' + leftText + '</a></li>';
     }
 
     // ========= Page Numbers Middle ======
@@ -67,9 +81,9 @@ exports.createPagination = function (pagination, options) {
     while (i < limit && i < pageCount) {
         n = start;
         if (start === page) {
-            template = template + '<li class="active"><a href="?page=' + n + queryParams + '">' + n + '</a></li>';
+            template = template + '<li class="page-item active"><a class="page-link" href="?page=' + n + queryParams + '">' + n + '</a></li>';
         } else {
-            template = template + '<li><a href="?page=' + n + queryParams + '">' + n + '</a></li>';
+            template = template + '<li class="page-item"><a class="page-link" href="?page=' + n + queryParams + '">' + n + '</a></li>';
         }
 
         start++;
@@ -77,15 +91,25 @@ exports.createPagination = function (pagination, options) {
     }
 
     // ========== Next Button ===========
-    if (page === pageCount || pageCount === 0) {
+    if (page === pageCount) {
         n = pageCount;
-        template = template + '<li class="disabled"><a href="#">'+ rightText +'</i></a></li>';
+        template = template + '<li class="page-item disabled"><a class="page-link" href="?page=' + n + queryParams + '">' + rightText + '</i></a></li>';
+    } else {
+        if (page >= pageCount) {
+            n = pageCount;
+        } else {
+            n = page + 1;
+        }
+        template = template + '<li class="page-item"><a class="page-link" href="?page=' + n + queryParams + '">' + rightText + '</a></li>';
     }
-    else {
-        n = page + 1;
-        template = template + '<li><a href="?page=' + n + queryParams + '">'+ rightText +'</a></li>';
+    
+    // ========= Last Button ===============
+    if (page === pageCount) {
+        template = template + '<li class="page-item disabled"><a class="page-link" href="?page=' + pageCount + queryParams + '">' + lastText + '</a></li>';
+    } else {
+        template = template + '<li class="page-item"><a class="page-link" href="?page=' + pageCount + queryParams + '">' + lastText + '</a></li>';
     }
+
     template = template + '</ul>';
     return template;
-}
-;
+};
